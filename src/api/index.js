e2e00ad84d2ca87f819b6d24d1d350bc2e78644b
Xianping Tao <20091578@mail.wit.ie>
2021-04-09 22:06:08 +0100

    Some api changes

--- src/api/index.js
+++ src/api/index.js
@@ -1,14 +1,10 @@
-// A module containing all interface request functions in the application
+export const login = (username, password) => {
+    return fetch('/api/users', {
+        headers: {
+            'Content-Type': 'application/json'
+        },
+        method: 'post',
+        body: JSON.stringify({ username: username, password: password })
+    }).then(res => res.json())
+};
 
-import ajax from './ajax';
-
-
-
-// login
-// export function reqLogin(username,password){
-//     return ajax('/login', {username, password}, 'POST')
-// }
-export const reqLogin = (username, password) => ajax('/login', {username, password}, 'POST')
-
-// add user
-export const reqAddUser = (user) => ajax('/manage/user/add', user, 'POST')
