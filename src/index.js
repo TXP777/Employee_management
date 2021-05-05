8dc71ab196f7aeb445c88600718e50c3c1b4097d
Xianping Tao <20091578@mail.wit.ie>
2021-05-05 23:59:13 +0100

    Improve some functions

--- src/index.js
+++ src/index.js
@@ -2,6 +2,7 @@ import React from "react";
 import ReactDOM from "react-dom";
 import {BrowserRouter, Route, Switch} from 'react-router-dom';
 import AuthContextProvider from "./context/authContext";
+import PrivateRoute from './components/router/privateRouter';
 import Login from './pages/login/login';
 import Admin from './pages/admin/admin';
 
@@ -13,7 +14,7 @@ const App = () => {
             <AuthContextProvider>
               <Switch> {/**Match only one of the routes */}
                 <Route path='/login' component={Login}></Route>
-                <Route path='/' component={Admin}></Route>
+                <PrivateRoute path='/' component={Admin}></PrivateRoute>
                 
               </Switch>
               </AuthContextProvider>
